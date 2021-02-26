f508e13d9a6b32fe321d7627bd91e4c9ff3d5946
qwertys318 <343604@gmail.com>
2021-02-26 12:39:12 +0700

    Revert "close"
    This reverts commit 570a8e7de29f7131eca2b87019f13cccbb7e9a6c.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <v-app>
     <v-snackbar v-model="snack.isShow">{{ snack.text }}</v-snackbar>
-    <v-container>
-      <v-alert type="error">
-        Project Closed!
-        <br/>See details in official <a href="https://extraton.io" target="_blank">site</a>.
-      </v-alert>
-    </v-container>
+    <div>
+      <v-app-bar>
+        <v-icon left>mdi-diamond-stone</v-icon>
+        <v-toolbar-title>extraTON.Vouch</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn href="https://t.me/extraton" target="_blank" text outlined>
+          <v-icon left>mdi-telegram</v-icon>
+          <span>Support</span>
+        </v-btn>
+        <template v-slot:extension>
+          <v-tabs align-with-title>
+            <v-tabs-slider color="#272727"/>
+            <v-tab :to="{name: 'main'}" exact>Home</v-tab>
+            <v-tab :to="{name: 'about'}">About</v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+    </div>
+
+    <div class="content">
+      <router-view :search="search"/>
+    </div>
   </v-app>
 </template>
 
